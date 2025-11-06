1fbda0aec5e4b4e3f5ce9cbdf1945ae9a3363f73
morenoben343colostate <benmore@colostate.edu>
2025-11-06 15:20:46 -0700

    Starter code for PR Calc

--- PageRank.java
+++ PageRank.java
@@ -11,7 +11,23 @@ import scala.Tuple2;
 
 
 public class PageRank {
-    // float d = 0.85; damping factor in Pagerank score equation
-    // int N; total number of nodes in PageRank score equation
-    // int sumScores = ??; Sum of PageRank scores from all linking nodes Q, divided by their outgoing links L(Q)
+    // Calculation for PageRank algorithm, takes in int amount of iterations (25) and a double for the damping factor we wish to use (0.85)
+    public static JavaPairRDD<Integer, Double> calculatePageRank(int iterations, double damping) {
+    
+        Doc doc = Book.mapPairs(); 
+        JavaPairRDD<Integer, List<Integer>> links = doc.links;
+        JavaPairRDD<Integer, String> titles = doc.title;
+        long N = links._2().length();
+
+        // Caches Links because they are reused for every iteration of the calculation
+        links.cache();
+
+        //initialize rank of each page to 1.0
+        JavaPairRDD<Integer, Double> ranks = links.keys().mapToPair(page -> new Tuple2<>(page, 1.0));
+        
+        //Loop PageRank Iteratons
+        for (int i = 0; i < iterations; i++ ) {
+            
+        }
+    }
 }
