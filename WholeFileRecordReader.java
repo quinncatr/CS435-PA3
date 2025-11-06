e4b30aae387d6564af54ef929c6a3f3ef1bfbaf2
morenoben343colostate <benmore@colostate.edu>
2025-11-05 23:57:29 -0700

    Added helper stuff and beginning pseudocode

---
+++ WholeFileRecordReader.java
@@ -0,0 +1,69 @@
+package tfidf; //needs to be changed to reflect our project
+
+//figure out if there is Spark versions of these import statements
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class WholeFileRecordReader extends RecordReader<NullWritable, BytesWritable> {
+	private FileSplit fileSplit;
+	private Configuration conf;
+	private BytesWritable value = new BytesWritable();
+	private boolean processed = false;
+
+	@Override
+	public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+		// #TODO#: Initialize the fileSplit and conf variables using the provided
+		// parameters
+		this.fileSplit = (FileSplit)split;
+		this.conf = context.getConfiguration();
+	}
+
+	@Override
+	public boolean nextKeyValue() throws IOException, InterruptedException {
+		if (!processed) {
+			byte[] contents = new byte[(int) fileSplit.getLength()];
+			Path file = fileSplit.getPath();
+			FileSystem fs = file.getFileSystem(conf);
+			FSDataInputStream in = null;
+			try {
+				in = fs.open(file);
+				IOUtils.readFully(in, contents, 0, contents.length);
+				value.set(contents, 0, contents.length);
+			} finally {
+				IOUtils.closeStream(in);
+			}
+			processed = true;
+			return true; // #TODO#: Return appropriate boolean value when processing is done
+		}
+		return false; // #TODO#: Return appropriate boolean value when no more data to process
+	}
+
+	@Override
+	public NullWritable getCurrentKey() throws IOException, InterruptedException {
+		return NullWritable.get();
+	}
+
+	@Override
+	public BytesWritable getCurrentValue() throws IOException, InterruptedException {
+		return value;
+	}
+
+	@Override
+	public float getProgress() throws IOException {
+		return processed ? 1.0f : 0.0f;
+	}
+
+	@Override
+	public void close() throws IOException {
+	}
+}
